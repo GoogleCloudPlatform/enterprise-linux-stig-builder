14a860a410793f83325ea1a5534e512b15fd7d55
Nick Sabine <nsabine@google.com>
2023-10-11 19:31:08 -0400

    Add Google Cloud Ops Agent to image to enable compliant log collection and storage.

--- build-image/additional-configurations.sh
+++ build-image/additional-configurations.sh
@@ -15,4 +15,30 @@
 
 # Any additional configuration needed after reboot can be executed here
 
+# Install the Guest Environment
+#  https://cloud.google.com/compute/docs/images/guest-environment
+
+eval $(grep VERSION_ID /etc/os-release)
+sudo tee /etc/yum.repos.d/google-cloud.repo << EOM
+[google-compute-engine]
+name=Google Compute Engine
+baseurl=https://packages.cloud.google.com/yum/repos/google-compute-engine-el${VERSION_ID/.*}-x86_64-stable
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+      https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
+sudo yum makecache
+sudo yum updateinfo
+sudo yum install -y google-compute-engine google-osconfig-agent
+
+
+# Install the Google Cloud Ops Agent
+#  https://cloud.google.com/stackdriver/docs/solutions/agents/ops-agent/installation
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
 echo "${0} script complete."
